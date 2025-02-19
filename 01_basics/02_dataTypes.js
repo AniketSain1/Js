a42c16b7340033374565077b0d9cc2c04acd00c0
AniketSain1 <100271717+AniketSain1@users.noreply.github.com>
2025-02-19 07:07:40 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+// alert("hello") // we are using nodejs, not browser
+/* console(3
+    +
+    3
+)*/ //code readablility should be high
+
+console.log("hi")
+
+
+let name ="hi"
+let age = 19
+let isLoggedIn = true
+let state = null
+
+
+// number =>2 to the power 53
+// bigint
+// string => "hi"
+// boolean=> true/false
+// null => standalone value
+//undefined => 
+// symbol => uinque
+
+
+
+// object
+
+console.log(typeof undefined)
