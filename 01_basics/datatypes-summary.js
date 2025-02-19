141344d6396b7dee9ba59e5039c760a0a4342507
AniketSain1 <100271717+AniketSain1@users.noreply.github.com>
2025-02-19 11:52:32 +0000

    datatype-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,45 @@
+// Primitive datatype
+
+// 7 type : String, Number, Booleon, null, undefined, symbol, Bigint
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const bigNumber = 326487985145851n
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+
+// Reference (Non primitive)
+// array, Object, Function
+
+
+const heros = ["Shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Aniket",
+    age: 25,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+
+/* typeof Operator Result
+undefined - "undefined"
+Null      - "Object"
+Boolean   - "Boolean"
+Number    - "Number"
+String    -  "String"
+Object    - "Object"  (where native and does not implement[[Call]])
+Object    - "Function"  (where native or host and does implement[[Call]])
+Object (host and does not implement [[Call]]) -  Implementation-defined except may not be 
+                                                 "undefined", "boolean", "number", or "string".
+*/
+
