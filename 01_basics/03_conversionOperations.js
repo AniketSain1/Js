7bb698ab0468c3925adc1cf9f4a14cbdafa18fe7
AniketSain1 <100271717+AniketSain1@users.noreply.github.com>
2025-02-19 08:00:42 +0000

    conversion of datatypes 2

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,5 +24,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber) 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//*****************************Operations*********************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+//console.log(str3);  // hello world
+
+//console.log("2" + 2); // 22
+//console.log(2 + "2");// 22 
+//console.log( "1" + 2 + 2);// 122
+//console.log(1 + 2 + "2");//32 
+
+//console.log(+true);  //1
+//console.log(true+);// error
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2 ;
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//https://tc39.es/ecma262/multipage/abstract-operations.html
